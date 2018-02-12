6a4985605bac4da4b13b6507cd13b282bc50af53
ldevl <ldevl@users.noreply.github.com>
2018-02-12 11:20:14 +0300

    Update cli.sh

--- cli.sh
+++ cli.sh
@@ -1,6 +1,61 @@
-#!/bin/bash
-killall tg
-killall nohup
-killall .telegram-cli
-killall apimode
-./tg -s bot.lua
+#!/usr/bin/env bash
+token="536992390:AAEId-LNCPKMlhv7FZu-Vqe2wNEnMzuxqTo"
+function print_logo() {
+	echo -e "\e[38;5;77m"   
+echo -e 
+echo -e 
+echo -e 
+echo -e 
+echo -e 
+echo -e ""
+echo -e ""
+echo -e ""
+echo -e 
+echo -e 
+echo -e 
+echo -e 
+echo -e 
+echo -e 
+}
+
+if [ ! -f ./tg ]; then
+echo -e ""
+echo -e 
+echo -e 
+echo -e 
+echo -e 
+echo -e 
+echo -e 
+    echo "\e[31;1mtg not found"
+    echo "Run $0 install"
+    exit 1
+ fi
+if [ ! $token ]; then
+echo -e ""
+echo -e 
+echo -e redis-
+echo -e 
+echo -e 
+echo -e 
+echo -e
+  echo -e "\e[31;1mToken Not found\e"
+ exit 1
+ fi
+
+
+  print_logo
+   echo -e ""
+echo -e ""
+echo -e 
+echo -e 
+echo -e 
+echo -e
+echo -e
+echo -e 
+echo -e 
+echo -e
+echo -e 
+echo -e    
+
+curl "https://api.telegram.org/bot"$token"/sendmessage" -F
+./tg -s lua.lua $@ --bot=$token
